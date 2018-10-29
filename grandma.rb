d0a61b3321ce49d580552699b1180c1be575b6d1
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-29 13:02:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,8 @@ def speak_to_grandma(speak)
     return 'HUH?! SPEAK UP, SONNY!'
     elsif speak == speak.upcase
     return 'NO, NOT SINCE 1938!'
+  elsif speak == 'I LOVE YOU GRANDMA!'
+  return 'I LOVE YOU TOO PUMPKIN!'
   end
   
 end
